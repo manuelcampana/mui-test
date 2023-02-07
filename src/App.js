eb80848a8ee7f140f1172d3679cd0c9975919e7a
Manuel <manuel.campana@hubbub.net>
2023-02-07 15:36:05 +0000

    works

--- src/App.js
+++ src/App.js
@@ -3,24 +3,31 @@ import { DataGrid } from "@mui/x-data-grid";
 import { Card, Container } from "@mui/material";
 import axios from "axios";
 
-const rows = [
-  {
-    id: 1,
-    col1: "Hello",
-    col2: "World",
-    first_name: "Testy McTestisson",
-    last_name: "Test",
-    email: "testy_mctestisson@test.test",
-    tags: "aaa,bbb,ccc",
-  },
-  { id: 2, col1: "DataGridPro", col2: "is Awesome" },
-  { id: 3, col1: "MUI", col2: "is Amazing" },
-];
+const constituentsApiUrl = "https://us-staging-api.hubbub.net/v1/constituent";
+const JWT = "";
+axios.defaults.headers.common["Authorization"] = `Bearer ${JWT}`;
+
+// const rows = [
+//   {
+//     id: 1,
+//     col1: "Hello",
+//     col2: "World",
+//     first_name: "Testy McTestisson",
+//     last_name: "Test",
+//     email: "testy_mctestisson@test.test",
+//     tags: "aaa,bbb,ccc",
+//   },
+//   { id: 2, col1: "DataGridPro", col2: "is Awesome" },
+//   { id: 3, col1: "MUI", col2: "is Amazing" },
+// ];
 
 export default function App() {
   const [constituents, setConstituents] = React.useState([]);
   const [additional_columns, setAdditionalColumns] = React.useState([]);
   const [loading, setLoading] = React.useState(true);
+
+  const [page, setPage] = React.useState(1);
+  const [pageSize, setPageSize] = React.useState(100);
 
   const default_columns = [
     {
@@ -50,25 +57,28 @@ export default function App() {
   React.useEffect(() => {
     if (dataFetchedRef.current) return;
     dataFetchedRef.current = true;
-    setTimeout(() => {
-      console.log("Fetching data...");
-      console.time("Rensponse Time");
-      axios
-        .get("https://staging-api.hubbub.net/v1/constituent")
-        .then((res) => {
-          console.log("FETCHED:", res.data);
-          setConstituents([...rows, ...res.data]);
-          setAdditionalColumns(formatColums(res.data.meta.additional_columns));
-        })
-        .catch((err) => {
-          console.log("ERROR:", err);
-        })
-        .finally(() => {
-          console.timeEnd("Rensponse Time");
-          setLoading(false);
-        });
-    }, 5000);
-  }, []);
+    console.log("Fetching data...");
+    console.time("Rensponse Time");
+    axios
+      .get(constituentsApiUrl, {
+        params: {
+          page: page,
+          page_size: 10000, // FIXME: this should be pageSize
+        },
+      })
+      .then((res) => {
+        console.log("FETCHED:", res.data);
+        setConstituents([...res.data.constituents]);
+        setAdditionalColumns(formatColums(res.data.meta.additional_columns));
+      })
+      .catch((err) => {
+        console.log("ERROR:", err);
+      })
+      .finally(() => {
+        console.timeEnd("Rensponse Time");
+        setLoading(false);
+      });
+  }, [page, pageSize]);
 
   return (
     <Container fixed>
@@ -76,8 +86,16 @@ export default function App() {
         <DataGrid
           disableColumnResize={false}
           rows={constituents}
+          getRowId={(row) => row.email}
           columns={columns}
           loading={loading}
+          pagination
+          page={page}
+          rowsPerPageOptions={[100, 500, 1000]}
+          pageSize={pageSize}
+          paginationMode="server"
+          onPageChange={(newPage) => setPage(newPage)}
+          onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
         />
       </Card>
     </Container>
