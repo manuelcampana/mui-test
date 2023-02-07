188df3ef2ed2e7b94140b55b311682a66c0f129e
Manuel <manuel.campana@hubbub.net>
2023-02-07 15:59:40 +0000

    hide footer + comment useless stuff

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import { Card, Container } from "@mui/material";
 import axios from "axios";
 
 const constituentsApiUrl = "https://us-staging-api.hubbub.net/v1/constituent";
-const JWT = "";
+const JWT =
+  "eyJraWQiOiJrT3kxcmloT0dFMEliRlloeWV1WFI2WnlzS1RJNEFUZDVjY3ZscnV6VXFvPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiI3ZDRlMjE1OC1jZmZhLTQyYmItOWIxYy04YmI1M2E5ZGJiMGMiLCJldmVudF9pZCI6ImJmYzM2OThiLWQ1NDgtNDczMS05YTU1LTg1YWRhMTQzMWViOCIsInRva2VuX3VzZSI6ImFjY2VzcyIsInNjb3BlIjoiYXdzLmNvZ25pdG8uc2lnbmluLnVzZXIuYWRtaW4iLCJhdXRoX3RpbWUiOjE2NzU3ODIzMTUsImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC51cy1lYXN0LTEuYW1hem9uYXdzLmNvbVwvdXMtZWFzdC0xX0JOMFRKaEJjVCIsImV4cCI6MTY3NTc4NTkxNSwiaWF0IjoxNjc1NzgyMzE1LCJqdGkiOiI1MzkwZmE3Ny1hM2YzLTQ0ODYtOGQ3Zi0zMDA4ZmVkMWNhMWYiLCJjbGllbnRfaWQiOiIyMnZ1NGU1bXBwczlnMG8yY3UxMWduZnYxMiIsInVzZXJuYW1lIjoiN2Q0ZTIxNTgtY2ZmYS00MmJiLTliMWMtOGJiNTNhOWRiYjBjIn0.stftqhXMVYxAum7xwjKjsfVABgunJZPBXUpQIgopC-oKHB30WMbFg9QbNZwzdWySHtguHn6oD7HvuBSaPfSdlA3i4clpeHJv4WqIP1RNiKFIp75tSxrIimkJGN5kTTbbAHZ6rxlM3F3Z7dY1lBBCcKziIFd2-igd5UwUo30PF1Xogn7UvxYCUe7GDsluFVFSNyCCgvfFhD4hgJ9-9GyttQAmJd2qAVDvut3-P7iuk2ybkjFntJdFguqrLgSmdHvs2J4fva3Y0cNBiD7KArgBRio20QWstLVzhBT3vPbIvk4F-9f8CQgYBQBA0jouPodHK7FCQxzd1LPvTvy-FZc1qw";
 axios.defaults.headers.common["Authorization"] = `Bearer ${JWT}`;
 
 // const rows = [
@@ -82,20 +83,21 @@ export default function App() {
 
   return (
     <Container fixed>
-      <Card style={{ marginTop: 50, height: "calc(100vh - 100px)" }}>
+      <Card style={{ marginTop: 50, height: "calc(100vh - 1000px)" }}>
         <DataGrid
           disableColumnResize={false}
           rows={constituents}
           getRowId={(row) => row.email}
           columns={columns}
           loading={loading}
-          pagination
-          page={page}
-          rowsPerPageOptions={[100, 500, 1000]}
-          pageSize={pageSize}
+          // pagination
+          // page={page}
+          // rowsPerPageOptions={[100, 500, 1000]}
+          // pageSize={pageSize}
           paginationMode="server"
-          onPageChange={(newPage) => setPage(newPage)}
-          onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+          // onPageChange={(newPage) => setPage(newPage)}
+          // onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+          hideFooter
         />
       </Card>
     </Container>
