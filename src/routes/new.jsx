2554866992811c8da60ef786fda33920801a4fa9
Manuel <manuel.campana@hubbub.net>
2023-02-16 16:28:21 +0000

    moved stuff to .env

--- src/routes/new.jsx
+++ src/routes/new.jsx
@@ -3,10 +3,8 @@ import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import { Card, Container } from "@mui/material";
 import axios from "axios";
 
-const constituentsApiUrl = "https://us-staging-api.hubbub.net/v1/constituent";
-const JWT =
-  "eyJraWQiOiJrT3kxcmloT0dFMEliRlloeWV1WFI2WnlzS1RJNEFUZDVjY3ZscnV6VXFvPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiI3ZDRlMjE1OC1jZmZhLTQyYmItOWIxYy04YmI1M2E5ZGJiMGMiLCJldmVudF9pZCI6IjM4YjUzZTZiLTBhM2QtNGQ0Ni04ZWU2LTdiYjM4ZmMyZjdlNCIsInRva2VuX3VzZSI6ImFjY2VzcyIsInNjb3BlIjoiYXdzLmNvZ25pdG8uc2lnbmluLnVzZXIuYWRtaW4iLCJhdXRoX3RpbWUiOjE2NzU3ODgyNTQsImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC51cy1lYXN0LTEuYW1hem9uYXdzLmNvbVwvdXMtZWFzdC0xX0JOMFRKaEJjVCIsImV4cCI6MTY3NTc5MTg1NCwiaWF0IjoxNjc1Nzg4MjU0LCJqdGkiOiJkZDVkMTdkNy0wNmVjLTQwNDMtOGQzOS00ZjAyYjQwMDUwZjgiLCJjbGllbnRfaWQiOiIyMnZ1NGU1bXBwczlnMG8yY3UxMWduZnYxMiIsInVzZXJuYW1lIjoiN2Q0ZTIxNTgtY2ZmYS00MmJiLTliMWMtOGJiNTNhOWRiYjBjIn0.RZQSu-fKngLDMq_rC2AjosdsNLR_sxzxQOfhE2QKFLeMmgjps5cVypcq_e1hVBK1QYxvbqNGs2dkLBL5Z78e7EtLlY1TOD4dHCJUJR74FIbGqh-UBODMCRcvevQptn8iogvI857B1lT7TSY_eusIdzE5KIDR6Yzqc2T0IivkVk8qXy9eF-Poup3hOK7foMZ0Aue4faXwsALy3PgHGHn2c8jzbniN9INkX3FioJVZnzuIFPmB3HrDIIBjEGe2-kdvcz5FcU-Qzc-eGuSo65VTpKZyYxlLtFRyFig5lQDsDlZlKZk0xUqnHopKHX8jTX0tZPT9gbW4IOBy4J6tY_KW6w";
-axios.defaults.headers.common["Authorization"] = `Bearer ${JWT}`;
+const { REACT_APP_JWT, REACT_APP_CONSTITUENTS_API_URL } = process.env;
+axios.defaults.headers.common["Authorization"] = `Bearer ${REACT_APP_JWT}`;
 
 // const rows = [
 //   {
@@ -63,7 +61,7 @@ export default function New() {
     console.log("Fetching data...");
     console.time("Rensponse Time");
     axios
-      .get(constituentsApiUrl, {
+      .get(REACT_APP_CONSTITUENTS_API_URL, {
         params: {
           page: page,
           page_size: 10000, // FIXME: this should be pageSize
--- src/routes/old.jsx
+++ src/routes/old.jsx
@@ -3,16 +3,13 @@ import { Card, Container } from "@mui/material";
 import MUIDataTable from "mui-datatables";
 import axios from "axios";
 
-const constituentsApiUrl = "https://us-staging-api.hubbub.net/v1/constituent";
-const JWT =
-  "eyJraWQiOiJrT3kxcmloT0dFMEliRlloeWV1WFI2WnlzS1RJNEFUZDVjY3ZscnV6VXFvPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiI3ZDRlMjE1OC1jZmZhLTQyYmItOWIxYy04YmI1M2E5ZGJiMGMiLCJldmVudF9pZCI6IjhmMDIyNjVmLWVlODYtNDg1Yi05NjBiLTI1ZjQ0ODU2ZGExMCIsInRva2VuX3VzZSI6ImFjY2VzcyIsInNjb3BlIjoiYXdzLmNvZ25pdG8uc2lnbmluLnVzZXIuYWRtaW4iLCJhdXRoX3RpbWUiOjE2NzY0NzAyMTksImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC51cy1lYXN0LTEuYW1hem9uYXdzLmNvbVwvdXMtZWFzdC0xX0JOMFRKaEJjVCIsImV4cCI6MTY3NjQ3MzgxOSwiaWF0IjoxNjc2NDcwMjE5LCJqdGkiOiIwYTc2ODA1ZC03NzQ0LTQ5YTItOGE5ZC1hMjM1ZDg0MTEwNGMiLCJjbGllbnRfaWQiOiIyMnZ1NGU1bXBwczlnMG8yY3UxMWduZnYxMiIsInVzZXJuYW1lIjoiN2Q0ZTIxNTgtY2ZmYS00MmJiLTliMWMtOGJiNTNhOWRiYjBjIn0.yZuU_LrbBgMsZQpUCADQp60LkHF7aIGk6LeUU6-phS1X94wdpeGY-V04P8B_L8ud1rNLzLSTiqKXDJutk0uba4fC1aExtoXtWy8YaIjt0rKZsTixznkEFswSpSd5a5bIFNKetBwrolVEV7dxN0uWNjI5vq4t-lOKaeI7ayyLwInE04fmPrQztLAswB3ClkGA2eEfjTtE5n5xMFoBYotKzBQ5rSlo9pzd-j9XS4N9sXbqrHF-nbqRU3c8rPoX0T3MnqJSExtda0VRmDdTB7MsfPJj9QqlEWFFGmSzaOtTReOnT1qg3Er24-Cwa8pYqIlLIbsLBD-fpk-LbhTeOZoC3A";
-axios.defaults.headers.common["Authorization"] = `Bearer ${JWT}`;
+const { REACT_APP_JWT, REACT_APP_CONSTITUENTS_API_URL } = process.env;
+axios.defaults.headers.common["Authorization"] = `Bearer ${REACT_APP_JWT}`;
 
 export default function Old() {
   const [constituents, setConstituents] = React.useState([]);
   const [additional_columns, setAdditionalColumns] = React.useState([]);
   const [loading, setLoading] = React.useState(true);
-
   const columns = [
     {
       name: "first_name",
@@ -35,6 +32,7 @@ export default function Old() {
 
   const options = {
     filterType: "checkbox",
+    tableBodyHeight: "calc(100vh - 300px)",
   };
 
   const dataFetchedRef = React.useRef(false);
@@ -45,7 +43,7 @@ export default function Old() {
     console.log("Fetching data...");
     console.time("Rensponse Time");
     axios
-      .get(constituentsApiUrl, {
+      .get(REACT_APP_CONSTITUENTS_API_URL, {
         params: {
           page: 1,
           page_size: 10000, // FIXME: this should be pageSize
